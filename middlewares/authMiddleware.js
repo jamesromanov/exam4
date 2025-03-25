83e49f366f2f2813b90983fdf4b4c0c7c7379515
James romanov <qahramonovjmashid1@gmail.com>
2025-03-25 18:13:47 +0500

    done

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -7,14 +7,13 @@ const protector = async (req, res, next) => {
   try {
     let tokenChecking = req.headers.authorization;
     if (!tokenChecking || !tokenChecking.startsWith("Bearer"))
-      throw new Error("Token xato!");
+      response(res, "Token olinmadi!", 401);
     let token = tokenChecking.split(" ")[1];
     let checking = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_KEY);
     let author = await Author.findById(checking.id).select(
       "role email isActive"
     );
-    if (!author || !author.isActive)
-      throw new Error("Foydalanuvchi topilmadi!");
+    if (!author || !author.isActive) response(res, "Token olinmadi!", 401);
     req.author = author;
     next();
   } catch (error) {
